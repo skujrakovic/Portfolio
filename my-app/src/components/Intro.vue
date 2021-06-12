01678f7e10cef5d86dbcee6f9dc2fba295612ba0
Samija Kujrakovic <skujrakovi1@etf.unsa.ba>
2021-06-12 12:29:02 +0200

    Added initial introduction

---
+++ my-app/src/components/Intro.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="introduction">
+    <h1>Hi, my name is Samija</h1>
+    <p>I am a Bachelor of Engineering, Computer Science and Informatics.</p>
+    <p>I have a diverse set of skills, ranging from frontend development using React.js and Vue.js to backend using Node.js</p>
+    <p>I love learning and creating new things.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Intro'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.introduction {
+    background-color: lavender;
+}
+h1 {
+  margin: 40px 0 0;
+}
+</style>
